2514d58b0b8f189c18e4cc5a0f709b0973beecd1
AnoukRImola <anouk138@gmail.com>
2021-02-09 22:29:32 +0000

    proyecto react semaforo

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //include bootstrap npm library into the bundle
-import "bootstrap";
+//import "bootstrap";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
+
+import TrafficLight from "./component/TrafficLight.jsx";
 
 //import your own components
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
